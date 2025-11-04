23f7481400f4c42b02825472a6dadeb813523b51
YuvrajSin2003 <yuvrajraghuvanshisingh@gmail.com>
2025-11-04 09:24:13 +0530

    complete the feed

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 
-const UserCard = ({user}) => {
-    const {firstName , lastName , photoUrl , age , gender , about } = user; 
+const UserCard = ({ user }) => {
+  const { firstName, lastName, photoUrl, age, gender, about } = user;
   return (
-    <div className="card bg-base-100 w-96 shadow-sm">
+    <div className="card bg-base-300 w-96 shadow-sm">
       <figure>
-        <img
-          src= {user.photoUrl}
-          alt="User photo"
-        />
+        <img src={photoUrl} alt="User photo" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{firstName + " " + lastName}</h2>
-        <p>
-          {about}
-        </p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary"></button>
+        <p>{about}</p>
+        { age && gender && <p>{age + " ," + gender}</p>}
+        <div className="card-actions justify-center my-4">
+          <button className="btn btn-primary">Ignore</button>
+          <button className="btn btn-secondary">Interested</button>
         </div>
       </div>
     </div>
