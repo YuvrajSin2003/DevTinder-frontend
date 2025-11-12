10897c38b3f917d002a6fbf3695d063644a05030
YuvrajSin2003 <yuvrajraghuvanshisingh@gmail.com>
2025-11-12 16:09:24 +0530

    build sign up page within the login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,16 +6,18 @@ import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("yuvi@gmail.com");
-  const [password, setpassword] = useState("Yuvi@9919");
-  const [error , setError] = useState("");
+  const [emailId, setEmailId] = useState("");
+  const [password, setpassword] = useState("");
+  const [error, setError] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isLogin, setLogin] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handelLogin = async () => {
     try {
       const res = await axios.post(
-        
         BASE_URL + "/login",
         {
           emailId,
@@ -31,12 +33,49 @@ const Login = () => {
       setError(err?.response?.data || "Something went wrong");
     }
   };
+  const handleSignUp = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        { firstName, lastName, emailId, password },
+        { withCredentials: true }
+      );
+      dispatch(addUser(res.data.data));
+      return navigate("/profile");
+    } catch (err) {
+      setError(err?.response?.data || "Something went wrong");
+    }
+  };
   return (
     <div className="flex justify-center items-center my-10">
       <div className="card bg-base-300 w-96 shadow-sm  ">
         <div className="card-body">
-          <h2 className="card-title justify-center">Login</h2>
+          <h2 className="card-title justify-center">
+            {isLogin ? "Sign Up" : "Login"}{" "}
+          </h2>
           <div>
+            {isLogin && (
+              <>
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">First Name</legend>
+                  <input
+                    type="text"
+                    className="input"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                </fieldset>
+                <fieldset className="fieldset">
+                  <legend className="fieldset-legend">Last Name</legend>
+                  <input
+                    type="text"
+                    className="input"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </fieldset>
+              </>
+            )}
             <fieldset className="fieldset">
               <legend className="fieldset-legend">Email ID?</legend>
               <input
@@ -50,7 +89,7 @@ const Login = () => {
             <fieldset className="fieldset">
               <legend className="fieldset-legend">Password//</legend>
               <input
-                type="text"
+                type="password"
                 className="input"
                 value={password}
                 onChange={(e) => setpassword(e.target.value)}
@@ -59,10 +98,22 @@ const Login = () => {
           </div>
           <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center">
-            <button className="btn btn-primary" onClick={handelLogin}>
-              Login
+            <button
+              className="btn btn-primary"
+              onClick={isLogin ?  handleSignUp : handelLogin
+
+              }
+            >
+              {isLogin ? "Sign Up" : "Login"}
             </button>
           </div>
+          <p
+            className="m-auto cursor-pointer py-2"
+            onClick={() => setLogin((value) => !value)}
+          >
+            {" "}
+            {isLogin ? "New User? Signuo here" : "Existing User? Login here"}
+          </p>
         </div>
       </div>
     </div>
